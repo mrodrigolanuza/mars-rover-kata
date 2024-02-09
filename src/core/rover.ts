32cde0f9bcafcdd92140d489a87c693cd4eca896
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-09 18:16:09 +0100

    Green: must be instantiated in a well-formed world and in a particular position in it

--- src/core/rover.ts
+++ src/core/rover.ts
@@ -1,10 +1,10 @@
 import { World } from "./world";
 
 export class Rover {
-    private constructor(private readonly world: World) {}
+    private constructor(private readonly world: World, xPos :number, yPos :number, orientation :string) {}
 
-    static create (world :World) :Rover{
+    static create (world :World, xPos :number, yPos :number, orientation :string) :Rover{
 
-        return new Rover(world);
+        return new Rover(world, xPos, yPos, orientation);
     }
 }
