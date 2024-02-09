7241a806474ea9b1a6be13b02d79b6c82361aee2
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-09 21:23:31 +0100

    Add TDD use cases

--- src/tests/rover.test.ts
+++ src/tests/rover.test.ts
@@ -1,3 +1,19 @@
+/*
+    - The Rover vehicle must be instantiated in a well-formed world and in a particular position in it.
+    - The Rover vehicle can not be instantiated if the position is not contained within the world dimensions.
+    - The Rover can show its actual position.
+    - The Rover can execute a movement with only a Forward command (F) within the world dimensions.
+    - The Rover can execute a movement with only a Rear command (R) within the world dimensions.
+    - The Rover can execute a movement with only a Left command (L) within the world dimensions.
+    - The Rover can execute a movement with only a Right command (R) within the world dimensions.
+    - The Rover can execute a movement with a sequece of commands (FRFFR) within the world dimensions.
+    - The Rover can execute a movement with only a Forward command (F) traspassing the limits of the world.
+    - The Rover can execute a movement with only a Rear command (R) traspassing the limits of the world.
+    - The Rover can execute a movement with only a Left command (L) traspassing the limits of the world.
+    - The Rover can execute a movement with only a Right command (R) traspassing the limits of the world.
+    - The Rover can execute a movement with a sequece of commands (FRFFR) traspassing the limits of 1x1 world.
+*/
+
 import { Rover } from '../core/rover'
 import { World } from '../core/world';
 
