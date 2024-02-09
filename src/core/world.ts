4b3c2b562c65f35f2678a5682f2fdc6cf85cb0eb
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-09 17:58:31 +0100

    Green: World can not be instantiated with negative Y dimension

--- src/core/world.ts
+++ src/core/world.ts
@@ -4,9 +4,12 @@ export class World {
 
     static create(dimX :number, dimY :number) :World {
         const MINIMUN_SIZE = 1;
-        
+
         if(dimX < MINIMUN_SIZE)
 			throw new Error("X dimension must be greather than zero.")
+
+        if(dimY < MINIMUN_SIZE)
+			throw new Error("Y dimension must be greather than zero.")
 
         return new World(dimX, dimY);
     }
