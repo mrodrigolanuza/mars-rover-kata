65ed7752a8da89cffd85e60b312451219c91d45d
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-09 18:06:45 +0100

    Green: The rover vehicle can be instantiated

---
+++ src/core/rover.ts
@@ -0,0 +1,11 @@
+import { World } from "./world";
+
+export class Rover {
+    private constructor(private readonly world: World) {}
+
+    static create (world :World) :Rover{
+
+        return new Rover(world);
+
+    }
+}
