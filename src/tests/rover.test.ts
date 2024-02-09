e7f1f89053d6383c1286712f41eb8b094cd0da90
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-09 18:10:59 +0100

    Refactor: The rover vehicle must be instantiated in a well-formed world

--- src/tests/rover.test.ts
+++ src/tests/rover.test.ts
@@ -3,9 +3,9 @@ import { World } from '../core/world';
 
 describe('The Rover vehicle', () => {
 
-	it('can be instantiated.', () => {
+    it('must be instantiated in a well-formed world.', () => {
 		
-        const world = World.create(3, 5)
+		const world = World.create(3, 5)
 
 		const rover = Rover.create(world);
 		
