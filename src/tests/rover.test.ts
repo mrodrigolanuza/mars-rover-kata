d9764e17e679e2c1b7af10394e0fe7c4592d4aaf
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-02-09 18:14:02 +0100

    Red: must be instantiated in a well-formed world and in a particular position in it

--- src/tests/rover.test.ts
+++ src/tests/rover.test.ts
@@ -3,11 +3,13 @@ import { World } from '../core/world';
 
 describe('The Rover vehicle', () => {
 
-    it('must be instantiated in a well-formed world.', () => {
+    it('must be instantiated in a well-formed world and in a particular position in it.', () => {
+        const xPos = 3;
+        const yPos = 5;
+        const orientation = 'N';
+        const world = World.create(3, 5)
 		
-		const world = World.create(3, 5)
-
-		const rover = Rover.create(world);
+        const rover = Rover.create(world, xPos, yPos, orientation);
 		
 		expect(rover).toBeInstanceOf(Rover);
 	});
